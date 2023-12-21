c3856536db3edc157fbc4db787fe3c19e56406ea
Venkat Sai Kedari Nath <venkatsaikedarinath@gmail.com>
2023-12-21 16:48:35 +0530

    adding packages and changes to index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const express = require("express")
+const app = express();
+function isoldenough(req, res, next) {
+    const age = req.query.age;
+    if (age >= 14) {
+        next();
+    } else {
+        res.json({
+            msg: "you are not yet age"
+        })
+    }
+}
+app.use(isoldenough)
+app.get("/ride2", (req, res, next) => {
+    res.json({
+        msg: "your ticket has been booked for the ride 2"
+    })
+
+})
+app.get("/ride1", (req, res, next) => {
+    res.json({
+        msg: "your ticket has been booked for the ride 1"
+    })
+})
+app.listen(3001)
